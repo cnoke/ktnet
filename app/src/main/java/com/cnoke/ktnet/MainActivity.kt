43fba3f0c7b71a00b9db8d2daa60f40773826cd2
huanghui <1093757107@qq.com>
2022-01-10 12:58:09 +0800

    删除无用代码

--- app/src/main/java/com/cnoke/ktnet/MainActivity.kt
+++ app/src/main/java/com/cnoke/ktnet/MainActivity.kt
@@ -1,21 +1,8 @@
 package com.cnoke.ktnet
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.cnoke.base.bean.ApiResponse
-import com.cnoke.base.MyGsonResponseBodyConverter
 import com.cnoke.base.activity.BaseActivity
-import com.cnoke.net.await
-import com.cnoke.net.factory.ApiResultCallAdapterFactory
-import com.cnoke.net.factory.GsonConverterFactory
-import com.cnoke.net.interception.HeadInterceptor
-import com.cnoke.net.interception.LogInterceptor
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
 
 /**
  * @date on 2022/1/7
